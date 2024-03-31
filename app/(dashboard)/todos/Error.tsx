0d109ee30141b940ed26952b142310ab0c201183
georgesimos <admin@georgesimos.com>
2024-03-31 18:33:48 +0300

    server actions

--- app/(dashboard)/todos/Error.tsx
+++ app/(dashboard)/todos/Error.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-export default function Error() {
+export default function ErrorPage() {
   return (
     <div>
       <h1>Todo not found</h1>
--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,8 +1,19 @@
+'use client';
+
+import { completeTodo } from '@/utils/actions';
 import { Todo } from '@prisma/client';
+import { useTransition } from 'react';
 
 export default function Todo({ todo }: { todo: Todo }) {
+  const [isPending, startTransition] = useTransition();
+
   return (
-    <div>
+    <div
+      className={`border border-white/20 cursor-pointer p-4 my-4 ${
+        todo.completed ? 'line-through text-gray-10' : ''
+      }`}
+      onClick={() => startTransition(() => completeTodo(todo.id))}
+    >
       <h1>{todo.content}</h1>
     </div>
   );
